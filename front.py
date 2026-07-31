19a9d3a62588a644d568227323f6f6216dc18b0c
Elvin <elvinpk@yahoo.ro>
2026-07-31 19:08:09 +0300

    zzzz

--- front.py
+++ front.py
@@ -12,18 +12,9 @@ css = '''<style>
     button[data-baseweb="tab"] p {font-size:55px!important;}
     # .st-c1 {background-color: green!important; border-color: green!important;}    # todo fix?
     div[data-baseweb="tab-highlight"] {background-color: blue!important;}
-    # label[data-baseweb="checkbox"] span, div.st-emotion-cache-bqwma9.ew2p8o3, div.st-emotion-cache-xjxhw3.ew2p8o3 {width:2rem;height:2rem;}
+    label[data-baseweb="checkbox"] span, div.st-emotion-cache-bqwma9.ew2p8o3, div.st-emotion-cache-xjxhw3.ew2p8o3 {width:2rem;height:2rem;}
     .stMainBlockContainer {padding:0 20px}
     .stAppHeader, ._container_gzau3_1, ._viewerBadge_nim44_23 {display: none;}
-    label[data-baseweb="checkbox"] {
-        min-height: 50px;
-        cursor: pointer;
-    }
-        
-    label[data-baseweb="checkbox"] span {
-        width: 28px;
-        height: 28px;
-    }
 '''
 
 # region Specifications
