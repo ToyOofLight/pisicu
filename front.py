67dbfb9daeae9d182c21eeb26c93bd4c39d505bf
Elvin <elvinpk@yahoo.ro>
2026-08-27 12:22:43 +0300

    fix checkboxes size 3

--- front.py
+++ front.py
@@ -12,7 +12,7 @@ css = '''<style>
     button[data-baseweb="tab"] p {font-size:55px!important;}
     div[data-baseweb="tab-highlight"] {background-color: blue!important;}
     # .stCheckbox label > div:first-child, .stCheckbox label > span {width:2rem;height:2rem;} # todo uncomm
-    .stCheckbox label > div:first-child {width:2rem;height:2rem;} # todo remove
+    .stCheckbox label div:first-child {width:2rem;height:2rem;} # todo remove
     .stMainBlockContainer {padding:0 20px}
     .stAppHeader, ._container_gzau3_1, ._viewerBadge_nim44_23 {display: none;}
     
